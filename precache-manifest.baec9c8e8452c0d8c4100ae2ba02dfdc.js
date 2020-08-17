98af8a5dca36bd0e830b5a1e2fbfb8dbfc5b94d1
Geoff Arroyo <garroyo@stedwards.edu>
2020-08-16 20:58:59 -0500

    Updates

---
+++ precache-manifest.baec9c8e8452c0d8c4100ae2ba02dfdc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "261070d6580551e746f85055f90a25f9",
+    "url": "/portfolio-pages/index.html"
+  },
+  {
+    "revision": "92a7a33e3dd256062467",
+    "url": "/portfolio-pages/static/css/main.259b7499.chunk.css"
+  },
+  {
+    "revision": "659b93e672533194b372",
+    "url": "/portfolio-pages/static/js/2.c3b78927.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/portfolio-pages/static/js/2.c3b78927.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92a7a33e3dd256062467",
+    "url": "/portfolio-pages/static/js/main.6d444806.chunk.js"
+  },
+  {
+    "revision": "0fa17c28a3cf289b7ebb",
+    "url": "/portfolio-pages/static/js/runtime-main.f83e58bf.js"
+  }
+]);
